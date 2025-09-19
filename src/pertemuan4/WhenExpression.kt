87985165883fc2f3021a0eb1214a635d83289240
michaelg62 <michaelgunawan_2226250062@mhs.mdp.ac.id>
2025-09-19 19:51:44 +0700

    lanjut

--- src/pertemuan4/WhenExpression.kt
+++ src/pertemuan4/WhenExpression.kt
@@ -1,28 +1,43 @@
 package pertemuan4
 
-fun main () {
+fun main() {
     val finalExam = 'A'
+    var nilai = 0
+    var kuis = 'A'
 
     when(finalExam) {
         'A' -> println("Lulus")
         'B' -> println("Lulus juga")
         'C' -> println("Ya bisa lulus")
-        else -> println("Tidak lulus")
+        else -> println("Tidak Lulus")
     }
-    when(finalExam) {
-        'A','B' -> println("Lulus")
-        'C' -> println("Ya bisa lulus")
-        else -> println("Tidak lulus")
-    }
-    when {
-        finalExam =='A' || finalExam == 'B' -> println("Lulus")
-        finalExam =='C' -> println("Ya bisa lulus")
-        else ->println("Tidak Lulus")
-    }
-    var contoh  = 5
 
-    when{
-        (contoh > 0) -> contoh = 13
+    when(finalExam) {
+        'A', 'B' -> println("Lulus")
+        'C' -> println("Ya bisa lulus")
+        else -> {
+            println("Tidak Lulus")
+        }
     }
-    println(contoh)
+
+    when {
+        finalExam == 'A' || finalExam == 'B' -> println("Lulus")
+        finalExam == 'C' -> println("Ya bisa lulus")
+        else -> println("Tidak lulus")
+    }
+
+    when (kuis){
+        'A' -> nilai = 80
+        'B' -> nilai = 60
+        'C' -> nilai = 40
+        else -> nilai = 20
+    }
+
+    println("Nilai : " + nilai)
+
+    when(finalExam){
+        is Char -> println("ya ini char")
+        is String -> println("ya ini string")
+        !is Char -> println("bukan char")
+    }
 }
